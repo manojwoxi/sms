7f960c7af48fe13092b4183eb7316bcb50543810
Amol Rokade <amol.woxi@gmail.com>
2015-12-01 10:56:17 +0530

    migrations

---
+++ database/migrations/2015_12_01_050359_create_student_tables.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStudentTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('div_id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('student');
+    }
+}
